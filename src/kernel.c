9da0b3180bef9eb5e7a0f49cd3b0df2dabcd424e
root <root@Jim.superawesome>
2024-02-24 22:36:52 -0500

    added the code I removed back into cstrcmp

--- src/kernel.c
+++ src/kernel.c
@@ -143,6 +143,11 @@
 
     // Function to compare two strings
     int cstrcmp(const char* str1, const char* str2) {
+        while (*str1 && (*str1 == *str2)) {
+            str1++;
+            str2++;
+        }
+
         return *(unsigned char*)str1 - *(unsigned char*)str2;
     }
 
